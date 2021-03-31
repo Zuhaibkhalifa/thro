2d5584d079caa3a61fcabceea1383507e9c758aa
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-03-31 23:00:30 +0530

    Minor changes won't break anything and Restructured the code

--- server/app/Pat_section_seven.php
+++ server/app/Pat_section_seven.php
@@ -6,15 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pat_section_seven extends Model
 {
-    
-
-
-       protected $fillable = [
-
-      'sec_seven_id',  'cognitive_heart_failure', 'high_blood_pressure', 'diabetes', 'mitral_stenosis',
-
-      'stroke_or_mini_stroke',  'cognitive_heart_failure', 'not_sure', 'none_of_above', 'user_id',
-    ];
+  protected $fillable = [
+    'sec_seven_id',  'cognitive_heart_failure', 'high_blood_pressure', 'diabetes', 'mitral_stenosis',
+    'stroke_or_mini_stroke', 'not_sure', 'none_of_above', 'user_id',
+  ];
 
 public $timestamps = false;
   public $table = "pat_section_seven";
