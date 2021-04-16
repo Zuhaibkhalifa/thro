7f68e9faf800d7faaace1d9b830d13b1c5e8e896
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-04-16 17:13:43 +0530

    Syntax Errors and few variables were missing in Models

--- server/app/Nurse_section_one.php
+++ server/app/Nurse_section_one.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nurse_section_one extends Model
 {
-  
   protected $fillable = [
     'section_one_id ',  'date', 'referred_by', 'procedure', 'cabg', 'user_id'
   ];
